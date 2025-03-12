9e8c62be6f6b93e19cc03bbc6be62ba5a3481b96
anshita <anshita05jai@gmail.com>
2025-03-12 12:39:50 +0530

    changes made

--- hello.cpp
+++ hello.cpp
@@ -5,6 +5,6 @@ using namespace std;
 int main(void) {
   cout << "Hello, World 2\n";
   cout << "Hello, Jenkins 2\n";
-  cout << "I have successfully built and run this 2s \n";
+  cout << "I have successfully built and run this 2\n";
   return 0;
 }
