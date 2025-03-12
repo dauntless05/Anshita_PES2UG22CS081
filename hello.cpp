ca4bff55d203ed9d31b8c70f6a39704658842be7
anshita <anshita05jai@gmail.com>
2025-03-12 12:37:05 +0530

    changes made

---
+++ hello.cpp
@@ -0,0 +1,10 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World 2\n";
+  cout << "Hello, Jenkins 2\n";
+  cout << "I have successfully built and run this 2s \n";
+  return 0;
+}
